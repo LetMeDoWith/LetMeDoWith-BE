e43f3bb0517db380a47e9abf341acc9930c37a03
SJ Kim <hedro0458@gmail.com>
2025-02-26 22:57:20 +0900

    feat: Todotask 도메인 repository

---
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/domain/task/repository/TodoTaskRoutineRepository.java
@@ -0,0 +1,8 @@
+package com.LetMeDoWith.LetMeDoWith.domain.task.repository;
+
+import com.LetMeDoWith.LetMeDoWith.domain.task.model.TodoTaskRoutine;
+
+public interface TodoTaskRoutineRepository {
+    
+    TodoTaskRoutine save(TodoTaskRoutine todoTaskRoutine);
+}
---
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/infrastructure/task/repository/TodoTaskRoutineRepositoryImpl.java
@@ -0,0 +1,19 @@
+package com.LetMeDoWith.LetMeDoWith.infrastructure.task.repository;
+
+import com.LetMeDoWith.LetMeDoWith.domain.task.model.TodoTaskRoutine;
+import com.LetMeDoWith.LetMeDoWith.domain.task.repository.TodoTaskRoutineRepository;
+import com.LetMeDoWith.LetMeDoWith.infrastructure.task.jpaRepository.TodoTaskRoutineJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class TodoTaskRoutineRepositoryImpl implements TodoTaskRoutineRepository {
+    
+    private final TodoTaskRoutineJpaRepository jpaRepository;
+    
+    @Override
+    public TodoTaskRoutine save(TodoTaskRoutine todoTaskRoutine) {
+        return jpaRepository.save(todoTaskRoutine);
+    }
+}
