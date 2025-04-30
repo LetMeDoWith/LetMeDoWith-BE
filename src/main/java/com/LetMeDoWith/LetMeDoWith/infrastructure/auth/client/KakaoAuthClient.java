dd1eeb73a089e41ea82cfec0f18e6b71defe2e40
SJ Kim <hedro0458@gmail.com>
2025-04-30 15:33:06 +0900

    fix: KakaoAuthClient dev 환경에서도 컴포넌트 등록되도록 수정

--- src/main/java/com/LetMeDoWith/LetMeDoWith/infrastructure/auth/client/KakaoAuthClient.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/infrastructure/auth/client/KakaoAuthClient.java
@@ -5,7 +5,6 @@ import com.LetMeDoWith.LetMeDoWith.application.auth.dto.OidcPublicKeyResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,7 +13,6 @@ import reactor.core.publisher.Mono;
 @Component
 @RequiredArgsConstructor
 @CacheConfig(cacheNames = "KAKAO", cacheManager = "socialProviderPublicKeyCacheManager")
-@Profile("!dev")
 public class KakaoAuthClient implements AuthClient {
     
     private final WebClient webClient;
