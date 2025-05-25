dd0ec64368f80d170c0ad05f826c756fe561e2ee
SJ Kim <hedro0458@gmail.com>
2025-05-25 11:02:47 +0900

    PR 대응

--- src/main/java/com/LetMeDoWith/LetMeDoWith/presentation/member/controller/MemberSettingController.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/presentation/member/controller/MemberSettingController.java
@@ -8,7 +8,7 @@ import com.LetMeDoWith.LetMeDoWith.common.annotation.ApiSuccessResponse;
 import com.LetMeDoWith.LetMeDoWith.common.dto.ResponseDto;
 import com.LetMeDoWith.LetMeDoWith.common.exception.status.FailResponseStatus;
 import com.LetMeDoWith.LetMeDoWith.common.util.ResponseUtil;
-import com.LetMeDoWith.LetMeDoWith.presentation.member.dto.UpdateMemberNotiSettingReq;
+import com.LetMeDoWith.LetMeDoWith.presentation.member.dto.UpdateMemberNotiSettingReqDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -23,9 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/members/settings")
 @RequiredArgsConstructor
 public class MemberSettingController {
-
+    
     private final MemberSettingService memberSettingService;
-
+    
     /**
      * 유저의 푸쉬 알람 수신 상태를 변경한다.
      *
@@ -37,9 +37,9 @@ public class MemberSettingController {
     @ApiErrorResponses({@ApiErrorResponse(status = FailResponseStatus.MEMBER_NOT_EXIST)})
     @PutMapping("/notification")
     public <T> ResponseEntity<ResponseDto<T>> updateNotificationSetting(
-            @RequestBody UpdateMemberNotiSettingReq req) {
+        @RequestBody UpdateMemberNotiSettingReqDto req) {
         memberSettingService.updateAlarmSetting(UpdateMemberAlarmSettingCommand.fromReq(req));
-
+        
         return ResponseUtil.createSuccessResponse();
     }
 }
