8605d0c8360a78943a045a1a6583d86d75977063
SJ Kim <hedro0458@gmail.com>
2025-06-04 21:38:59 +0900

    fix: TodoTaskRoutineSplitter 의 변수 명 수정

--- src/main/java/com/LetMeDoWith/LetMeDoWith/domain/task/service/TodoTaskRoutineSplitter.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/domain/task/service/TodoTaskRoutineSplitter.java
@@ -1,6 +1,5 @@
 package com.LetMeDoWith.LetMeDoWith.domain.task.service;
 
-
 import com.LetMeDoWith.LetMeDoWith.common.annotation.DomainService;
 import com.LetMeDoWith.LetMeDoWith.domain.task.model.TodoTask;
 import com.LetMeDoWith.LetMeDoWith.domain.task.model.TodoTaskRoutine;
@@ -9,43 +8,37 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-
-/**
- * TodoTask의 루틴을 분리하는 도메인 서비스.
- * TodoTask 루틴의 수정, 삭제시 이번만 적용 / 모두 적용의 여부에 따라 루틴을 분리한다.
- */
+/** TodoTask의 루틴을 분리하는 도메인 서비스. TodoTask 루틴의 수정, 삭제시 이번만 적용 / 모두 적용의 여부에 따라 루틴을 분리한다. */
 @DomainService
 public class TodoTaskRoutineSplitter {
     
     /**
-     * pivot을 기준으로 루틴을 분리한다.
-     * isApplyToAll이 true라면 루틴을 피봇 기준으로 분리한다.
-     * isApplyToAll이 false라면 피봇만 루틴에서 분리한다.
+     * pivot을 기준으로 루틴을 분할한다.
+     * pivot을 포함하여 미래의 TodoTask는 새로운 루틴에 포함되고, 이전의 TodoTask는 기존 루틴에서 분리된다.
      *
      * @param todoTasksInRoutine 루틴에 포함된 TodoTask 목록
      * @param pivot              루틴 분리의 기준이 되는 TodoTask
      * @param routine            루틴
      * @return 루틴 분리 결과 (루틴, 새로운 루틴의 TodoTask 목록, 분리된 TodoTask 목록)
      */
-    public TodoTaskRoutineSplitResult splitTodoTaskRoutine(List<TodoTask> todoTasksInRoutine,
-                                                           TodoTask pivot,
-                                                           TodoTaskRoutine routine) {
+    public TodoTaskRoutineSplitResult splitTodoTaskRoutine(
+        List<TodoTask> todoTasksInRoutine, TodoTask pivot, TodoTaskRoutine routine) {
         
-        List<TodoTask> old = new ArrayList<>();
-        List<TodoTask> future = new ArrayList<>();
+        List<TodoTask> todoTasksToBeDetached = new ArrayList<>();
+        List<TodoTask> todoTasksInNewRoutine = new ArrayList<>();
         
         for (TodoTask todoTask : todoTasksInRoutine) {
             if (todoTask.getDate().isBefore(pivot.getDate())) {
-                old.add(todoTask);
+                todoTasksToBeDetached.add(todoTask);
             } else {
-                future.add(todoTask);
+                todoTasksInNewRoutine.add(todoTask);
             }
         }
         
         // 오래된 것들은 루틴에서 분리
-        old.forEach(TodoTask::detachRoutine);
+        todoTasksToBeDetached.forEach(TodoTask::detachRoutine);
         
-        return TodoTaskRoutineSplitResult.of(routine, future, old);
+        return TodoTaskRoutineSplitResult.of(routine, todoTasksInNewRoutine, todoTasksToBeDetached);
     }
     
     @AllArgsConstructor
@@ -53,22 +46,20 @@ public class TodoTaskRoutineSplitter {
     public static class TodoTaskRoutineSplitResult {
         
         private TodoTaskRoutine newRoutine;
-        private List<TodoTask> futureTodoTasks;
+        private List<TodoTask> newTodoTasks;
         private List<TodoTask> detachedTodoTasks;
         
-        public static TodoTaskRoutineSplitResult of(TodoTaskRoutine newRoutine,
-                                                    List<TodoTask> futureTodoTasks,
-                                                    List<TodoTask> detachedTodoTasks) {
-            return new TodoTaskRoutineSplitResult(newRoutine,
-                                                  futureTodoTasks,
-                                                  detachedTodoTasks);
+        public static TodoTaskRoutineSplitResult of(
+            TodoTaskRoutine newRoutine,
+            List<TodoTask> newTodoTasks,
+            List<TodoTask> detachedTodoTasks) {
+            return new TodoTaskRoutineSplitResult(newRoutine, newTodoTasks, detachedTodoTasks);
         }
         
-        public static TodoTaskRoutineSplitResult of(TodoTaskRoutine newRoutine,
-                                                    List<TodoTask> futureTodoTasks,
-                                                    TodoTask detachedTodoTask) {
+        public static TodoTaskRoutineSplitResult of(
+            TodoTaskRoutine newRoutine, List<TodoTask> newTodoTasks, TodoTask detachedTodoTask) {
             return new TodoTaskRoutineSplitResult(newRoutine,
-                                                  futureTodoTasks,
+                                                  newTodoTasks,
                                                   List.of(detachedTodoTask));
         }
     }
