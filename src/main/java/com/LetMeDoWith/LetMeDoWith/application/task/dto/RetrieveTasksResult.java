e74901832ae70f4c6a474d357336808facb5f049
gyun_ky <kiyoon9390@gmail.com>
2025-06-04 21:24:56 +0900

    feat: dto 로직 가독성 강화

--- src/main/java/com/LetMeDoWith/LetMeDoWith/application/task/dto/RetrieveTasksResult.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/application/task/dto/RetrieveTasksResult.java
@@ -2,12 +2,13 @@ package com.LetMeDoWith.LetMeDoWith.application.task.dto;
 
 import com.LetMeDoWith.LetMeDoWith.infrastructure.task.query.dto.DowithTaskQueryDto;
 import com.LetMeDoWith.LetMeDoWith.infrastructure.task.query.dto.TodoTaskQueryDto;
+import lombok.Builder;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import lombok.Builder;
 
 public record RetrieveTasksResult(List<TodoTaskDto> todoTasks, List<DowithTaskDto> dowithTasks) {
 
@@ -27,36 +28,39 @@ public record RetrieveTasksResult(List<TodoTaskDto> todoTasks, List<DowithTaskDt
                                                 todoTaskQueryDto.startTime()))
                         .toList();
 
-        List<DowithTaskDto> dowithTasks =
+        List<DowithTaskDto> dowithTaskDtos =
                 dowithTaskQueryDtos.stream()
-                        .collect(
-                                Collectors.groupingBy(
-                                        DowithTaskQueryDto::id,
-                                        Collectors.collectingAndThen(
-                                                Collectors.toList(),
-                                                (List<DowithTaskQueryDto> list) -> {
-                                                    DowithTaskQueryDto first = list.get(0);
-                                                    List<String> confirmImageUrls =
-                                                            list.stream()
-                                                                    .map(DowithTaskQueryDto::confirmedImageUrl)
-                                                                    .filter(Objects::nonNull)
-                                                                    .toList();
-                                                    return new DowithTaskDto(
-                                                            first.id(),
-                                                            first.taskCategoryId(),
-                                                            first.taskCategoryName(),
-                                                            first.title(),
-                                                            first.status(),
-                                                            first.date(),
-                                                            first.startTime(),
-                                                            confirmImageUrls,
-                                                            first.feedBackCount());
-                                                })))
+                        .collect(Collectors.groupingBy(DowithTaskQueryDto::id))
                         .values()
                         .stream()
+                        .map(list -> {
+                            DowithTaskQueryDto first = list.get(0);
+                            List<String> confirmImageUrls = list.stream()
+                                    .map(DowithTaskQueryDto::confirmedImageUrl)
+                                    .filter(Objects::nonNull)
+                                    .toList();
+                            return new DowithTaskDto(
+                                    first.id(),
+                                    first.taskCategoryId(),
+                                    first.taskCategoryName(),
+                                    first.title(),
+                                    first.status(),
+                                    first.date(),
+                                    first.startTime(),
+                                    confirmImageUrls,
+                                    first.feedBackCount());
+                        })
+                        .sorted(
+                                (o1, o2) -> {
+                                    int dateComparison = o1.date().compareTo(o2.date());
+                                    if (dateComparison != 0) {
+                                        return dateComparison;
+                                    }
+                                    return o1.startTime().compareTo(o2.startTime());
+                                })
                         .toList();
 
-        return new RetrieveTasksResult(todoTasks, dowithTasks);
+        return new RetrieveTasksResult(todoTasks, dowithTaskDtos);
     }
 
     @Builder
@@ -67,7 +71,8 @@ public record RetrieveTasksResult(List<TodoTaskDto> todoTasks, List<DowithTaskDt
             String title,
             String status,
             LocalDate date,
-            LocalTime startTime) {}
+            LocalTime startTime) {
+    }
 
     public record DowithTaskDto(
             Long id,
@@ -78,5 +83,6 @@ public record RetrieveTasksResult(List<TodoTaskDto> todoTasks, List<DowithTaskDt
             LocalDate date,
             LocalTime startTime,
             List<String> confirmedImageUrls,
-            int feedBackCount) {}
+            int feedBackCount) {
+    }
 }
