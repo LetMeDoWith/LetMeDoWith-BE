50063076147180ac8c5eba3d51846c9068639a3a
gyun_ky <kiyoon9390@gmail.com>
2025-06-08 19:48:52 +0900

    feat: commit

--- src/main/java/com/LetMeDoWith/LetMeDoWith/presentation/task/dto/RetrieveTasksResDto.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/presentation/task/dto/RetrieveTasksResDto.java
@@ -4,10 +4,11 @@ import com.LetMeDoWith.LetMeDoWith.application.task.dto.RetrieveTasksResult;
 import com.LetMeDoWith.LetMeDoWith.domain.task.enums.DowithTaskStatus;
 import com.LetMeDoWith.LetMeDoWith.domain.task.enums.TodoTaskStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-import lombok.Builder;
 
 @Builder
 public record RetrieveTasksResDto(List<TodoTaskDto> todoTasks, List<DowithTaskDto> dowithTasks) {
@@ -49,23 +50,25 @@ public record RetrieveTasksResDto(List<TodoTaskDto> todoTasks, List<DowithTaskDt
     @Builder
     public record TodoTaskDto(
             @Schema(description = "투두모드Task ID", defaultValue = "1") Long id,
-            @Schema(description = "Task카테고리 ID", defaultValue = "2") Long taskCategoryId,
-            @Schema(description = "카테고리명", defaultValue = "일상") String taskCategoryName,
+            @Schema(description = "Task카테고리 ID", defaultValue = "2", nullable = true) Long taskCategoryId,
+            @Schema(description = "카테고리명", defaultValue = "일상", nullable = true) String taskCategoryName,
             @Schema(description = "제목", defaultValue = "아침 먹기") String title,
             @Schema(description = "상태", implementation = TodoTaskStatus.class) String status,
             @Schema(description = "일자", defaultValue = "2025-01-30") LocalDate date,
-            @Schema(description = "시작시간", defaultValue = "11:30:00") LocalTime startTime) {}
+            @Schema(description = "시작시간", defaultValue = "11:30:00") LocalTime startTime) {
+    }
 
     @Builder
     public record DowithTaskDto(
             @Schema(description = "두윗모드Task ID", defaultValue = "1") Long id,
-            @Schema(description = "Task카테고리 ID", defaultValue = "2") Long taskCategoryId,
-            @Schema(description = "카테고리명", defaultValue = "일상") String taskCategoryName,
+            @Schema(description = "Task카테고리 ID", defaultValue = "2", nullable = true) Long taskCategoryId,
+            @Schema(description = "카테고리명", defaultValue = "일상", nullable = true) String taskCategoryName,
             @Schema(description = "제목", defaultValue = "아침 먹기") String title,
             @Schema(description = "상태", implementation = DowithTaskStatus.class) String status,
             @Schema(description = "일자", defaultValue = "2025-01-30") LocalDate date,
             @Schema(description = "시작시간", defaultValue = "11:30:00") LocalTime startTime,
             @Schema(description = "인증 이미지 URL", defaultValue = "https://example.com/image.jpg")
-                    List<String> confirmedImageUrls,
-            @Schema(description = "피드백 개수", defaultValue = "5") int feedBackCount) {}
+            List<String> confirmedImageUrls,
+            @Schema(description = "피드백 개수", defaultValue = "5") int feedBackCount) {
+    }
 }
