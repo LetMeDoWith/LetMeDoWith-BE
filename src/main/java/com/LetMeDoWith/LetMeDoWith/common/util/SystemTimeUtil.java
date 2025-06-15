b5351179656c9785ea5cbb2ad49f3f56719076f7
gyun_ky <kiyoon9390@gmail.com>
2025-06-15 18:44:34 +0900

    feat: timezone filter 작업

--- src/main/java/com/LetMeDoWith/LetMeDoWith/common/util/SystemTimeUtil.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/common/util/SystemTimeUtil.java
@@ -1,15 +1,17 @@
 package com.LetMeDoWith.LetMeDoWith.common.util;
 
+import com.LetMeDoWith.LetMeDoWith.common.holders.TimeZoneContextHolder;
+import lombok.experimental.UtilityClass;
+
 import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class SystemTimeUtil {
 
-    private static Clock clock = Clock.systemDefaultZone();
+    private static Clock clock = Clock.system(TimeZoneContextHolder.getTimeZone());
 
     public static void setClock(Clock clock) {
         if (!ProfileHolder.activeProfile.equals("test")) {
@@ -19,7 +21,7 @@ public class SystemTimeUtil {
     }
 
     public static void resetClock() {
-        clock = Clock.systemDefaultZone();
+        clock = Clock.system(TimeZoneContextHolder.getTimeZone());
     }
 
     public static LocalDateTime now() {
