c65b8fa37c2c460ad26677c6aa4ada583ad4b424
gyun_ky <kiyoon9390@gmail.com>
2025-06-15 19:40:02 +0900

    feat: spotless

--- src/main/java/com/LetMeDoWith/LetMeDoWith/common/converter/DateTimeConverter.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/common/converter/DateTimeConverter.java
@@ -3,7 +3,6 @@ package com.LetMeDoWith.LetMeDoWith.common.converter;
 import com.LetMeDoWith.LetMeDoWith.common.holders.TimeZoneContextHolder;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
-
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -24,8 +23,6 @@ public class DateTimeConverter implements AttributeConverter<LocalDateTime, Time
         if (timestamp == null) return null;
 
         ZoneId timeZone = TimeZoneContextHolder.getTimeZone();
-        return timestamp.toInstant()
-                .atZone(timeZone)
-                .toLocalDateTime();
+        return timestamp.toInstant().atZone(timeZone).toLocalDateTime();
     }
 }
