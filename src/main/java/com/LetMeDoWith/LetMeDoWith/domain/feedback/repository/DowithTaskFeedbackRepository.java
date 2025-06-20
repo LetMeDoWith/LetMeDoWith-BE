90ac60fc7f67a0251ff7a6c92444a4d5519ca5fc
SJ Kim <hedro0458@gmail.com>
2025-06-20 22:51:12 +0900

    feat: feedback 도메인 모델 infrastructure layer

---
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/domain/feedback/repository/DowithTaskFeedbackRepository.java
@@ -0,0 +1,10 @@
+package com.LetMeDoWith.LetMeDoWith.domain.feedback.repository;
+
+import com.LetMeDoWith.LetMeDoWith.domain.feedback.model.DowithTaskFeedback;
+
+public interface DowithTaskFeedbackRepository {
+    
+    DowithTaskFeedback save(DowithTaskFeedback dowithTaskFeedback);
+    
+    
+}
---
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/infrastructure/feedback/query/DowithTaskFeedbackQueryDto.java
@@ -0,0 +1,7 @@
+package com.LetMeDoWith.LetMeDoWith.infrastructure.feedback.query;
+
+public record DowithTaskFeedbackQueryDto(
+// TODO: 잔소리 조회 스키마 정의
+) {
+
+}
