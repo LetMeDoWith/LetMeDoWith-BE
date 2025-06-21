d029f9f2a265f533b2d53211a0f52930c4a858e6
gyun_ky <kiyoon9390@gmail.com>
2025-06-22 00:28:26 +0900

    feat: timezone

--- src/main/java/com/LetMeDoWith/LetMeDoWith/common/converter/DateTimeConverter.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/common/converter/DateTimeConverter.java
@@ -3,26 +3,27 @@ package com.LetMeDoWith.LetMeDoWith.common.converter;
 import com.LetMeDoWith.LetMeDoWith.common.holders.TimeZoneContextHolder;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
-import java.sql.Timestamp;
+
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 @Converter(autoApply = true)
-public class DateTimeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+public class DateTimeConverter implements AttributeConverter<LocalDateTime, Instant> {
 
     @Override
-    public Timestamp convertToDatabaseColumn(LocalDateTime dateTime) {
+    public Instant convertToDatabaseColumn(LocalDateTime dateTime) {
         if (dateTime == null) return null;
 
         ZoneId timeZone = TimeZoneContextHolder.getTimeZone();
-        return Timestamp.from(dateTime.atZone(timeZone).toInstant());
+        return dateTime.atZone(timeZone).toInstant();
     }
 
     @Override
-    public LocalDateTime convertToEntityAttribute(Timestamp timestamp) {
-        if (timestamp == null) return null;
+    public LocalDateTime convertToEntityAttribute(Instant instant) {
+        if (instant == null) return null;
 
         ZoneId timeZone = TimeZoneContextHolder.getTimeZone();
-        return timestamp.toInstant().atZone(timeZone).toLocalDateTime();
+        return LocalDateTime.ofInstant(instant, timeZone);
     }
 }
