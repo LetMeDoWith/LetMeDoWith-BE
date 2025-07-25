85176aa38e51645c42b0424080c6d209c1b8454d
SJ Kim <hedro0458@gmail.com>
2025-07-25 20:02:36 +0900

    fix: FeedbackCreationPolicy에 대한 config

--- src/main/java/com/LetMeDoWith/LetMeDoWith/domain/feedback/service/TenMinutesFeedbackCreationPolicy.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/domain/feedback/service/TenMinutesFeedbackCreationPolicy.java
@@ -1,11 +1,9 @@
 package com.LetMeDoWith.LetMeDoWith.domain.feedback.service;
 
-import com.LetMeDoWith.LetMeDoWith.common.annotation.DomainService;
 import com.LetMeDoWith.LetMeDoWith.domain.feedback.model.DowithTaskFeedback;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-@DomainService
 public class TenMinutesFeedbackCreationPolicy implements FeedbackCreationPolicy {
     @Override
     public boolean isAdditionalFeedbackAvailable(Optional<DowithTaskFeedback> latestFeedback, LocalDateTime now) {
