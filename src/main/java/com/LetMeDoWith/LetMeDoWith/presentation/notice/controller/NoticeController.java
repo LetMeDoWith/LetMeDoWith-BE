eab53909b9287c55c6f11c7b7dc15322142117ef
SJ Kim <hedro0458@gmail.com>
2025-11-08 00:20:43 +0900

    delete: notice 생성 API 삭제 (admin용으로 개발)

--- src/main/java/com/LetMeDoWith/LetMeDoWith/presentation/notice/controller/NoticeController.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/presentation/notice/controller/NoticeController.java
@@ -1,19 +1,21 @@
 package com.LetMeDoWith.LetMeDoWith.presentation.notice.controller;
 
-import com.LetMeDoWith.LetMeDoWith.application.notice.dto.CreateNoticeCommand;
-import com.LetMeDoWith.LetMeDoWith.application.notice.service.CreateNoticeService;
+import com.LetMeDoWith.LetMeDoWith.application.notice.dto.RetrieveNoticesResult;
+import com.LetMeDoWith.LetMeDoWith.application.notice.service.RetrieveNoticeService;
 import com.LetMeDoWith.LetMeDoWith.common.annotation.ApiSuccessResponse;
-import com.LetMeDoWith.LetMeDoWith.common.dto.ResponseDto;
+import com.LetMeDoWith.LetMeDoWith.common.dto.ResponsePageDto;
+import com.LetMeDoWith.LetMeDoWith.common.enums.notice.NoticeType;
 import com.LetMeDoWith.LetMeDoWith.common.util.ResponseUtil;
-import com.LetMeDoWith.LetMeDoWith.presentation.notice.dto.CreateNoticeReqDto;
+import com.LetMeDoWith.LetMeDoWith.presentation.notice.dto.RetrieveNoticesResDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.ObjectUtils.Null;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Notice", description = "공지사항/이벤트")
@@ -22,21 +24,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class NoticeController {
 
-    private final CreateNoticeService createNoticeService;
+    private final RetrieveNoticeService retrieveNoticeService;
 
-    @Operation(summary = "공지사항/이벤트 생성", description = "새로운 공지사항/이벤트 게시글을 추가합니다")
-    @ApiSuccessResponse(description = "새로운 공지 추가 성공")
-    @PostMapping("")
-    public ResponseEntity<ResponseDto<Null>> createNotice(@RequestBody CreateNoticeReqDto req) {
 
-        createNoticeService.createNotice(CreateNoticeCommand.of(
-                req.type(),
-                req.title(),
-                req.content(),
-                req.startDateTime(),
-                req.endDateTime(),
-                req.thumbnailImageUrl()));
+    @Operation(summary = "공지사항/이벤트 목록 조회", description = "공지사항/이벤트 목록을 조회합니다. 페이징 가능")
+    @ApiSuccessResponse(description = "공지 조회")
+    @GetMapping("")
+    public ResponseEntity<ResponsePageDto<RetrieveNoticesResDto>> retrieveNotices(
+        @RequestParam(required = false) NoticeType type,
+        @ParameterObject Pageable pageable) {
 
-        return ResponseUtil.createSuccessResponse();
+        RetrieveNoticesResult result = retrieveNoticeService.retrieveNotices(type,
+            pageable);
+        RetrieveNoticesResDto res = RetrieveNoticesResDto.from(result);
+
+        return ResponseUtil.createSuccessResponse(res, pageable, result.totalCount());
     }
 }
