11925e418bd4d99e63338730bdc81754f2094f49
gyun_ky <kiyoon9390@gmail.com>
2025-11-16 15:49:46 +0900

    feat: spotless

--- src/main/java/com/LetMeDoWith/LetMeDoWith/batch/tasklet/UpdateFailDowithTaskTasklet.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/batch/tasklet/UpdateFailDowithTaskTasklet.java
@@ -1,6 +1,10 @@
 package com.LetMeDoWith.LetMeDoWith.batch.tasklet;
 
 import com.LetMeDoWith.LetMeDoWith.domain.task.enums.DowithTaskStatus;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.StepScope;
@@ -10,11 +14,6 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 @Component
 @StepScope
@@ -31,7 +30,8 @@ public class UpdateFailDowithTaskTasklet implements Tasklet {
 
         // 시작한지 1시간이 지난 DowithTask들을 FAIL로 업데이트
         LocalDate standardDate = executionDateTime.toLocalDate();
-        LocalTime standardTime = executionDateTime.toLocalTime().minusHours(1);
+        LocalTime standardTime =
+                executionDateTime.toLocalTime().minusHours(1).withSecond(59).withNano(0);
 
         int updatedCount = this.jdbcTemplate.update(
                 """
