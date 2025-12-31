35544377566e7530c68c2160fce6f9d81bda5d22
SJ Kim <hedro0458@gmail.com>
2025-12-31 16:04:00 +0900

    docs: swagger 주석 추가

--- src/main/java/com/LetMeDoWith/LetMeDoWith/presentation/feed/controller/FeedController.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/presentation/feed/controller/FeedController.java
@@ -2,15 +2,19 @@ package com.LetMeDoWith.LetMeDoWith.presentation.feed.controller;
 
 import com.LetMeDoWith.LetMeDoWith.application.feed.dto.RetrieveFeedbackAvailableDowithTasksResult;
 import com.LetMeDoWith.LetMeDoWith.application.feed.service.FeedService;
+import com.LetMeDoWith.LetMeDoWith.common.annotation.ApiSuccessResponse;
 import com.LetMeDoWith.LetMeDoWith.common.dto.ResponseDto;
 import com.LetMeDoWith.LetMeDoWith.common.util.ResponseUtil;
 import com.LetMeDoWith.LetMeDoWith.presentation.feed.dto.RetrieveFeedbackAvailableDowithTasksResDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Feed", description = "둘러보기")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/feeds")
@@ -18,11 +22,14 @@ public class FeedController {
 
     private final FeedService feedService;
 
+    @Operation(summary = "잔소리 대상 두윗 태스크 조회", description = "잔소리 대상 두윗 목록을 조회합니다.")
+    @ApiSuccessResponse(description = "잔소리 대상 두윗 목록 조회 성공")
     @GetMapping("/tasks/dowith")
     public ResponseEntity<ResponseDto<RetrieveFeedbackAvailableDowithTasksResDto>>
-            retrieveFeedbackAvailableDowithTasks() {
+    retrieveFeedbackAvailableDowithTasks() {
         RetrieveFeedbackAvailableDowithTasksResult result = feedService.retreiveFeedbackAvailableDowithTasks();
 
-        return ResponseUtil.createSuccessResponse(RetrieveFeedbackAvailableDowithTasksResDto.from(result));
+        return ResponseUtil.createSuccessResponse(
+            RetrieveFeedbackAvailableDowithTasksResDto.from(result));
     }
 }
--- src/main/java/com/LetMeDoWith/LetMeDoWith/presentation/feed/dto/RetrieveFeedbackAvailableDowithTasksResDto.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/presentation/feed/dto/RetrieveFeedbackAvailableDowithTasksResDto.java
@@ -2,10 +2,13 @@ package com.LetMeDoWith.LetMeDoWith.presentation.feed.dto;
 
 import com.LetMeDoWith.LetMeDoWith.application.feed.dto.RetrieveFeedbackAvailableDowithTasksResult;
 import com.LetMeDoWith.LetMeDoWith.application.feed.dto.RetrieveFeedbackAvailableDowithTasksResult.RetrieveFeedbackAvailableDowithTaskResult;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalTime;
 import java.util.List;
 
-public record RetrieveFeedbackAvailableDowithTasksResDto(List<RetrieveFeedbackAvailableDowithTaskDto> dowithTasks) {
+public record RetrieveFeedbackAvailableDowithTasksResDto(
+    @Schema(description = "잔소리 가능 두윗 태스크 목록")
+    List<RetrieveFeedbackAvailableDowithTaskDto> dowithTasks) {
 
     public static RetrieveFeedbackAvailableDowithTasksResDto from(RetrieveFeedbackAvailableDowithTasksResult result) {
 
@@ -14,14 +17,23 @@ public record RetrieveFeedbackAvailableDowithTasksResDto(List<RetrieveFeedbackAv
                 .toList());
     }
 
+    @Schema(description = "잔소리 가능 두윗 태스크 상세")
     public record RetrieveFeedbackAvailableDowithTaskDto(
+            @Schema(description = "태스크 ID", example = "1")
             Long id,
+            @Schema(description = "회원 ID", example = "test_member_id")
             String memberId,
+            @Schema(description = "닉네임", example = "nickname")
             String nickname,
+            @Schema(description = "뱃지 이미지 URL", example = "badge_url")
             String badgeImageUrl,
+            @Schema(description = "태스크 제목", example = "title")
             String title,
+            @Schema(description = "상태", example = "WAIT")
             String status,
+            @Schema(description = "시작 시간", example = "12:00:00")
             LocalTime startTime,
+            @Schema(description = "피드백 카운트", example = "0")
             Long feedbackCount) {
 
         public static RetrieveFeedbackAvailableDowithTaskDto from(RetrieveFeedbackAvailableDowithTaskResult result) {
