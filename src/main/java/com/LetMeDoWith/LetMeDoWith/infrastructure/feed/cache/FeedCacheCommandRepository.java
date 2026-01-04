5712d51c3d0d8e55d2e8868da824108a4dcfaea2
SJ Kim <hedro0458@gmail.com>
2026-01-04 23:28:47 +0900

    refactor: 배치의 Redis 접근 로직을 Repository 레이어를 통해 처리하도록 리팩토링

--- src/main/java/com/LetMeDoWith/LetMeDoWith/infrastructure/feed/cache/FeedCacheCommandRepository.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/infrastructure/feed/cache/FeedCacheCommandRepository.java
@@ -1,3 +1,9 @@
 package com.LetMeDoWith.LetMeDoWith.infrastructure.feed.cache;
 
-public interface FeedCacheCommandRepository {}
+import com.LetMeDoWith.LetMeDoWith.infrastructure.feed.query.dto.FeedbackAvailableDowithTaskQueryDto;
+import java.util.List;
+
+public interface FeedCacheCommandRepository {
+
+    void refreshFeedbackAvailableDowithTasks(List<FeedbackAvailableDowithTaskQueryDto> dowithTasks);
+}
