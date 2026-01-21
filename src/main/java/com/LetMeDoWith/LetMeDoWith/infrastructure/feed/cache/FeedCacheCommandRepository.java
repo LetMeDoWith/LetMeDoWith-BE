3ee9af51dadbd032b46af7d6fd5bc1994ec1d931
SJ Kim <hedro0458@gmail.com>
2026-01-21 21:06:13 +0900

    delete: feed cache 관련 repository 제거; db 직접 쿼리로 변경

--- src/main/java/com/LetMeDoWith/LetMeDoWith/infrastructure/feed/cache/FeedCacheCommandRepository.java
+++
@@ -1,9 +0,0 @@
-package com.LetMeDoWith.LetMeDoWith.infrastructure.feed.cache;
-
-import com.LetMeDoWith.LetMeDoWith.infrastructure.feed.query.dto.FeedDowithTaskQueryDto;
-import java.util.List;
-
-public interface FeedCacheCommandRepository {
-
-    void refreshFeedbackAvailableDowithTasks(List<FeedDowithTaskQueryDto> dowithTasks);
-}
