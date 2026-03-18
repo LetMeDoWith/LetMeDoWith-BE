4f738f1277d66dfb3c0f682d9b363a8431dee9b6
SJ Kim <hedro0458@gmail.com>
2026-03-18 22:18:10 +0900

    test: cover godsaeng ranking batch scenarios

--- src/main/java/com/LetMeDoWith/LetMeDoWith/infrastructure/ranking/persistence/jpaRepository/RankingTopicRoundJpaRepository.java
+++ src/main/java/com/LetMeDoWith/LetMeDoWith/infrastructure/ranking/persistence/jpaRepository/RankingTopicRoundJpaRepository.java
@@ -1,6 +1,9 @@
 package com.LetMeDoWith.LetMeDoWith.infrastructure.ranking.persistence.jpaRepository;
 
 import com.LetMeDoWith.LetMeDoWith.domain.ranking.model.RankingTopicRound;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RankingTopicRoundJpaRepository extends JpaRepository<RankingTopicRound, Long> {}
+public interface RankingTopicRoundJpaRepository extends JpaRepository<RankingTopicRound, Long> {
+    Optional<RankingTopicRound> findByRankingTopicIdAndRound(Long rankingTopicId, Long round);
+}
